c6831c7745dea01410a900a3cf66856f6c8e4ac7
KartikEngitech <ekta@kartikengitech.com>
2025-08-25 14:59:13 +0530

    new changes

--- teen_patti_backend/teen_patti_backend/urls.py
+++ teen_patti_backend/teen_patti_backend/urls.py
@@ -15,12 +15,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 from django.shortcuts import redirect
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Teen Patti API",
+      default_version='v1',
+      description="API documentation for Teen Patti project",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="support@teenpatti.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 
 def redirect_to_admin(request):
@@ -36,5 +53,11 @@ urlpatterns = [
     path('',include('adminapp.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+
+    # Swagger & Redoc
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
