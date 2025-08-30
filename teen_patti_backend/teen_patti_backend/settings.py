dad4ca89343d928e9b4f529ebecfdc2414ff5788
KartikEngitech <ekta@kartikengitech.com>
2025-08-30 18:46:58 +0530

    new changes for master table

--- teen_patti_backend/teen_patti_backend/settings.py
+++ teen_patti_backend/teen_patti_backend/settings.py
@@ -160,10 +160,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # ✅ for Render
 
 # Optional (but good)
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'teen_patti_backend/static')]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
--- teen_patti_backend/teen_patti_backend/urls.py
+++ teen_patti_backend/teen_patti_backend/urls.py
@@ -64,7 +64,7 @@ urlpatterns = [
     path('',include('adminapp.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include('cashmastertable.urls')),
 
     # Swagger & Redoc
