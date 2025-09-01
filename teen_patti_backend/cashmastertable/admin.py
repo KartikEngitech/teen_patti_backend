4e7388b3457ee8ac7359ee9db908ca09007be811
KartikEngitech <ekta@kartikengitech.com>
2025-09-01 10:19:54 +0530

    new changes for master table

--- teen_patti_backend/cashmastertable/admin.py
+++ teen_patti_backend/cashmastertable/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import MasterGameTable
 
-# Register your models here.
+
+@admin.register(MasterGameTable)
+class MasterGameTableAdmin(admin.ModelAdmin):
+    list_display = ('id', 'boot_price', 'max_bet_value', 'players', 'max_players')
+    search_fields = ('id', 'boot_price', 'max_bet_value')
+    list_filter = ('boot_price', 'max_players')
